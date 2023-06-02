d6d648bda55dcb935238869916d0136188c6851b
Jan Kallwies <jan@kallwies.com>
2023-06-02 23:08:45 +0200

    minigrep: Read args and read the input file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    dbg!(query);
+    dbg!(file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect(format!("Could not open the file {file_path}").as_str());
+
+    dbg!(contents);
 }
